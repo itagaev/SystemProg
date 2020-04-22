728a059fbdc958b831221976fbfef35431a5ca3d
adilsaidagali <adilsaidagali@gmail.com>
2020-04-22 13:34:59 +0500

    Adil changes

--- sis 2/sis2.c
+++ sis 2/sis2.c
@@ -1,85 +1,52 @@
-#include <linux/module.h>
-#include <linux/moduleparam.h>
-#include <linux/init.h>
-#include <asm/current.h>
-#include <linux/kernel.h>
+#include <linux/module.h> 
+#include <linux/init.h> 
+#include <linux/kernel.h> 
 #include <linux/sched.h>
 #include <linux/sched/signal.h>
+#include <asm/current.h>
 
-static int parameter = 0;
-module_param(parameter, int, 0660);
-
-//a
-struct mystruct {
-   unsigned int arrayOfIntegers[8];
-};
-
-//passing name, type and permission for module
-static int param = 2000; //default value = 2000.
-module_param(param, int, 0660);
-
-
-
-//
-
-int show_all_cpu(void)
-{
-    unsigned int ax = 11, bx = 0, cx = 1, dx = 0;
-    asm volatile("id of CPU"
-                 : "=a"(ax),
-                   "=b"(bx),
-                   "=c"(cx),
-                   "=d"(dx)
-                 : "0"(ax), "2"(cx)
-                 :);
-    return bx;
-}
-
-void filter_cpu(int cpu_id)
-{
+void cpu_information(int cpu_id) {
     struct task_struct *proc;
-    printk(KERN_INFO "All processes in CPU_ID %d:\n", cpu_id);
-    for_each_process(proc)
-    {
-        if (proc->cpu == cpu_id)
-        {
 
-            printk(KERN_INFO " PID: [%d].\n",  proc->pid);
+    printk(KERN_INFO "Processes of CPU #%d:\n", cpu_id);
+    char s[100] = "Name:", x[100] = "ID";
+    printk(KERN_INFO "\t%-20s[%s]\n", s, x);
+    for_each_process(proc) {
+    
+        if(proc->cpu == cpu_id) {
+            printk("\t%-20s[%d]", proc->comm, proc->pid);
         }
+    
     }
+
 }
 
-void print_processes(int s)
-{
-    printk(KERN_INFO "Start of the function:");
-    int i = 0;
-    int cpus = show_all_cpu();
-    if (s == 0)
-    {
-        filter_cpu(0);
+void proc_information(int k) {
+    unsigned int eax=11,ebx=0,ecx=1,edx=0;
+    asm volatile("cpuid"
+        : "=a" (eax),
+          "=b" (ebx),
+          "=c" (ecx),
+          "=d" (edx)
+        : "0" (eax), "2" (ecx)
+        : );
+    int i;
+    for(i = 0; i < ebx; i++){
+        cpu_information(i);
     }
-    if (s > 0 && s <= cpus)
-    {
-        for (i = 0; i < s; i++)
-        {
-            filter_cpu(i);
-        }
-    }
-    else if (cpus < s || s < 0)
-    {
-        printk(KERN_INFO "There is %d CPU cores.\nThere is no such CPU!\n", cpus, cpus - 1);
-    }
-    printk(KERN_INFO "End of the function\n");
+
 }
 
 int init_module(void)
 {
-    printk(KERN_INFO "Start of SIS2\n");
-    print_processes(parameter);
+    printk(KERN_INFO "Hi, there are information about your CPUs:\n");
+    proc_information(0);
+    printk(KERN_INFO "Bye, see you soon!\n\n\n");
     return 0;
 }
 
 void cleanup_module(void)
 {
-    printk(KERN_INFO "End of SIS2.\n");
+    printk(KERN_INFO "Bye, see you soon!\n");
 }
+
